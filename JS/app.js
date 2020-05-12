31b9aef5add1306ae606fb16313cd2be7a884dec
RB-Codes <rasheedbader8@gmail.com>
2020-05-12 07:15:07 +0300

    lab

---
+++ JS/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+alert('Welcome to the page')
+alert('Wanna start guessing?')
+var joinus = prompt('type (Sure/Later)')
+var joinusin = joinus.toLocaleLowerCase();
+console.log(joinusin);
+switch (joinus.toLocaleLowerCase()) {
+    case 'sure':
+        alert('Seems you are confident')
+        console.log('Seems you are confident')
+
+        break;
+    case 'later':
+        alert('As you like!')
+        console.log('user did not like the page')
+        break;
+    default:
+        alert('I have to know if you are sure')
+        console.log('You are not answering')
+
+        
+
+
+
+        function rightanswer() { }
+}
---
+++ JS/app2.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var uanswer = prompt('Only one of these things I can not do, now type letters O and K')
+var unanswer2 = uanswer.toLocaleUpperCase;
+console.log('we are doing it baby')
+switch (uanswer.toLocaleUpperCase()) {
+        case 'OK':
+                alert('you are amazing')
+                console.log('he said ok')
+                break;
+}
+alert('1.Rope skipping')
+alert('2.Play soccer')
+alert('3.Drifting')
+alert('4.Horse riding')
+
+var finalguess = prompt('Your guess is?')
+
+if (finalguess === '3'); {
+        alert('Congrats!') }
